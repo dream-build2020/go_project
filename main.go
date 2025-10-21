cd5157ac51c0b4b1379e349ef294a4b395b13bc4
kindtom123@gmail.com <kindtom123@gmail.com>
2025-10-21 17:47:29 +0800

    create a new branch

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"go_project/core"
+
+	"fmt"
+)
 
 func main() {
+	// this is test tools
 	fmt.Println("hello,world!")
+
+	data_1 := "first"
+	//
+	core.CorePrint(data_1)
 }
